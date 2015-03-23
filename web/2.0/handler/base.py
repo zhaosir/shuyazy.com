b8954fcf37943d9cdd9eb6c7cbb3d0aca8460a15
zhaosir <zhaozijian@outlook.com>
2015-03-23 14:54:53 +0800

    zzj

---
+++ web/2.0/handler/base.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import logging
+from tornado import web,gen
+
+class BaseHandler(web.RequestHandler):
+	pass
---
+++ web/2.0/handler/indexHandler.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+from tornado import web
+from .base import BaseHandler
+
+class IndexHandler(BaseHandler):
+	def get(self):
+		pass
