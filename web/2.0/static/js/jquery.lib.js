5c2f4f6f3fd96c7143853f43fbabdebd04727186
zhaosir <zhaozijian@outlook.com>
2015-04-17 07:02:04 +0800

    zzj

--- web/2.0/static/js/jquery.lib.js
+++ web/2.0/static/js/jquery.lib.js
@@ -1,7 +1,9 @@
 (function($) {
-	$.get_argument = function (name,defaultvalue=null) {
+	$.get_argument = function (name,defaultvalue) {
 		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
 		var r = window.location.search.substr(1).match(reg);
 		if (r != null) return unescape(r[2]); return defaultvalue;
 	}
 })(jQuery);
+
+
